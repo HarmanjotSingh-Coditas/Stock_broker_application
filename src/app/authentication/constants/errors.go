0a1f117e70b7480e20637ff96f88955912bde1ea
HarmanjotSingh-Coditas <harmanjot.singh@coditas.com>
2025-03-25 11:00:03 +0530

    Update errors.go

--- src/app/authentication/constants/errors.go
+++ src/app/authentication/constants/errors.go
@@ -6,7 +6,7 @@ const (
 	ErrInvalidEmail       = "Invalid Email ID. It should be in the format abcdefg@doamin.com"
 	ErrInvalidPhone       = "Invalid Phone Number. It should be of 10 digits in the format 9876543210."
 	ErrInvalidPAN         = "Invalid PAN format. It should be in the format ABCDE1234F."
-	ErrInvalidPassword    = "Invalid Password format. It should contain atleast one uppercae , one lowercase"
+	ErrInvalidPassword    = "Invalid Password format. It should contain atleast one uppercae , one lowercase and a special character"
 	ErrPasswordMismatch   = "Password Mismatched , password and confirmpassword must match"
 	ErrUserExists         = "user already exists"
 	ErrCreateUser         = "failed to create user"
